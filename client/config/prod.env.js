30a2ae5d165c140166461dbc8c152555c1e2b101
liucan <chuck7liu@gmail.com>
2016-09-22 18:07:20 +0800

    增加部署配置 增加client的私有config

--- client/config/prod.env.js
+++ client/config/prod.env.js
@@ -1,6 +1,14 @@
-module.exports = {
+var fs = require('fs');
+var _ = require('lodash');
+config = {
   NODE_ENV: '"production"',
   //填写生产环境时,该博客的访问地址
-  index:'"http://chuckliu.me"',
+  index:'"http://localhost"',
   api:'"http://localhost:3000/api/"'
 }
+if(process.env.NODE_ENV === 'production'){
+  if(fs.existsSync(__dirname+'/private.js')){
+    config = _.merge(config,require('./private.js'));
+  }
+}
+module.exports = config
---
+++ server/configs/private.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mongoConfig: {
+    url: 'mongodb://localhost:27017/kov-blog-production',
+    opts:{
+      user:'',
+      pass:''
+    }
+  },
+  'jwt': {
+    'cert': 'kov-blog-product'
+  },
+}
