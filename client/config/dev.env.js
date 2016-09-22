602701a01061bcea777cb26c47b67c14d57e4f35
liucan <chuck7liu@gmail.com>
2016-09-22 21:05:36 +0800

    增加多说styl

--- client/config/dev.env.js
+++ client/config/dev.env.js
@@ -7,5 +7,7 @@ module.exports = merge(prodEnv, {
   port:port,
   //填写开发环境时,该博客的访问地址:
   index:'"http://localhost:'+port+'"',
-  api:'"http://localhost:3000/api/"'
+  api:'"http://localhost:3000/api/"',
+  //请修改为你自己的多说二级域名,
+  duoshuoShortName:'"xiachufang"'
 })
