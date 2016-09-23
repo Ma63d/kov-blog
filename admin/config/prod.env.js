f048b3daf2b7333d421ab7f73ceebc7c2b08294b
liucan <chuck7liu@gmail.com>
2016-09-23 10:32:38 +0800

    admin增加环境设置

--- admin/config/prod.env.js
+++ admin/config/prod.env.js
@@ -1,4 +1,14 @@
-module.exports = {
+var fs = require('fs');
+var merge = require('webpack-merge')
+config = {
   NODE_ENV: '"production"',
   api:'"http://localhost:3000/api/"'
 }
+
+if(process.env.NODE_ENV === 'production'){
+  if(fs.existsSync(__dirname+'/private.js')){
+    config = merge(config,require('./private.js'));
+  }
+}
+module.exports = config
+
--- client/config/prod.env.js
+++ client/config/prod.env.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var _ = require('lodash');
+var merge = require('webpack-merge')
 config = {
   NODE_ENV: '"production"',
   //填写生产环境时,该博客的访问地址
@@ -8,7 +8,7 @@ config = {
 }
 if(process.env.NODE_ENV === 'production'){
   if(fs.existsSync(__dirname+'/private.js')){
-    config = _.merge(config,require('./private.js'));
+    config = merge(config,require('./private.js'));
   }
 }
 module.exports = config
